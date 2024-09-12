fb85a503c25b3924e00212ec027ad5c866f7bc65
Indra Palijama <indrapalijama@gmail.com>
2024-09-12 16:10:50 +0700

    Initial Commit

---
+++ Poromodo/PoromodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoromodoApp.swift
+//  Poromodo
+//
+//  Created by Samudra Palijama on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PoromodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
